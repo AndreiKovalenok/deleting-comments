2e3dbcbc79aa6e5bc69784938023c970bdc48e15
AndreiKovalenok <dusvich@gmail.com>
2017-09-20 17:24:21 +0300

    Update script.cpp

--- script.cpp
+++ script.cpp
@@ -1,49 +1,59 @@
-#include "stdafx.h" 
-#include <iostream> 
-#include <fstream> 
-#include <string> 
-#include <vector> 
-#include <iterator> 
+#include "stdio.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
 
-using namespace std; 
+using namespace std;
 
-int main() 
-{ 
-  vector<string> input_text; 
-  vector<string>::iterator it; 
-  fstream file; 
-  file.open("D:/codeNomUpgrade.c",ios::in); 
-  while (!file.eof()) 
-  { 
-    string tmp(""); 
-    getline(file, tmp); 
-    input_text.push_back(tmp); 
-  } 
-  for (it = input_text.begin(); it < input_text.end(); it++) cout « *it « endl; 
-  for (it = input_text.begin(); it < input_text.end(); it++) 
-  { 
-    if ((*it).find("//") != -1) 
-    { 
-      (*it).erase(); 
-    } 
-    else if ((*it).find("/*") != -1) 
-    { 
-      do 
-      { 
-        (*it).erase(); 
-        it++; 
-      }
-      while ((*it).find("*/") == -1); 
-      (*it).erase(); 
-    } 
-  } 
-
-  cout « "___________________________" « endl; 
-
-  for (it = input_text.begin(); it < input_text.end(); it++) cout « *it « endl; 
-
-  file.open("D:/codeNomUpgrade_result.c", ios::out); 
+void fnoncoments(string & onestr)
+{
+    ofstream fout;
+    fout.open("code_after.C", ios::in);
+    fout.seekp(0, ios::end);
+    
+    fout << onestr << endl;
+    fout.close();
+}
 
 
-  return 0; 
+int main()
+{
+    vector<string> input_text;
+    vector<string>::iterator it;
+    fstream file;
+    file.open("code_before.C",ios::in);
+    
+    while (!file.eof())
+    {
+        string tmp("");
+        getline(file, tmp);
+        input_text.push_back(tmp);
+    }
+    
+    for (it = input_text.begin(); it < input_text.end(); it++)
+    {
+        if ((*it).find("//") != -1)
+        {
+            (*it).erase((*it).find("//"),(*it).size());
+        }
+        else if ((*it).find("/*") != -1)
+        {
+            do
+            {
+                (*it).erase();
+                it++;
+            }
+            while ((*it).find("*/") == -1);
+            (*it).erase();
+        }
+    }
+    
+    for (it = input_text.begin(); it < input_text.end(); it++)
+    {
+        cout << *it << endl;
+        fnoncoments(*it);
+    }
+    return 0; 
 }
